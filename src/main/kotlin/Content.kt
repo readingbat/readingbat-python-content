cbe85f2150d2fd5baca4996cf5a8d723adf8238e
pambrose <pambrose@mac.com>
2020-05-03 14:56:20 -0700

    Update content

--- src/main/kotlin/Content.kt
+++ src/main/kotlin/Content.kt
@@ -1,61 +1,56 @@
+import com.github.readingbat.ReturnType.BooleanType
 import com.github.readingbat.dsl.readingBatContent
 
 val content =
-  readingBatContent {
+    readingBatContent {
 
-      python {
-          repoRoot = "https://github.com/readingbat/readingbat-python-content"
+        python {
+            repoRoot = "https://github.com/readingbat/readingbat-python-content"
 
-          group("Warmup 1") {
-              packageName = "warmup1"
-              description = "This is a description of Warmup 1"
+            group("Warmup 1") {
+                packageName = "warmup1"
+                description = "This is a description of Warmup 1"
 
-              challenge("simple_choice1") {
-                  description = "This is a description of **simple_choice1**"
+                challenge("simple_choice1") {
+                    description = "This is a description of **simple_choice1**"
+                    returnType = BooleanType
+                }
 
-                  true returns false
-                  false returns false
-              }
+                challenge("simple_choice2") {
+                    description = "This is a description of **simple_choice2**"
+                    codingBatEquiv = "p173401"
+                    returnType = BooleanType
+                }
+            }
 
-              challenge("simple_choice2") {
-                  description = "This is a description of **simple_choice2**"
-                  codingBatEquiv = "p173401"
+            group("Warmup 2") {
+                packageName = "warmup2"
+                description = "This is a description of Warmup 2"
+            }
 
-                  listOf(true, true) returns true
-                  listOf(true, false) returns true
-                  listOf(false, true) returns false
-                  listOf(false, false) returns true
-              }
-          }
-
-          group("Warmup 2") {
-              packageName = "warmup2"
-              description = "This is a description of Warmup 2"
-          }
-
-          group("Logic 1") {
-              packageName = "logic1"
-              description = "This is a description of Logic 1"
-          }
-          group("Logic 2") {
-              packageName = "logic2"
-              description = "This is a description of Logic 2"
-          }
-          group("String 1") {
-              packageName = "string1"
-              description = "This is a description of String 1"
-          }
-          group("String 2") {
-              packageName = "string2"
-              description = "This is a description of String 2"
-          }
-          group("Array 1") {
-              packageName = "array1"
-              description = "This is a description of Array 1"
-          }
-          group("Array 2") {
-              packageName = "array2"
-              description = "This is a description of Array 2"
-          }
-      }
-  }
+            group("Logic 1") {
+                packageName = "logic1"
+                description = "This is a description of Logic 1"
+            }
+            group("Logic 2") {
+                packageName = "logic2"
+                description = "This is a description of Logic 2"
+            }
+            group("String 1") {
+                packageName = "string1"
+                description = "This is a description of String 1"
+            }
+            group("String 2") {
+                packageName = "string2"
+                description = "This is a description of String 2"
+            }
+            group("Array 1") {
+                packageName = "array1"
+                description = "This is a description of Array 1"
+            }
+            group("Array 2") {
+                packageName = "array2"
+                description = "This is a description of Array 2"
+            }
+        }
+    }
