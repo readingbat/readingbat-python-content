5cf289720591eaa22af2bece6e2a1a5fd3262514
Matthew Ambrose <mattbobambrose@gmail.com>
2020-07-03 12:01:43 -0700

    This should work

---
+++ python/string_operations/concat1.py
@@ -0,0 +1,14 @@
+def concat1(s1, s2):
+    return s1 + s2
+
+
+def main():
+    print(concat1("Car", "wash"))
+    print(concat1("Hello", " world"))
+    print(concat1("5", "8"))
+    print(concat1("Snow", "ball"))
+    print(concat1("Rain", "boots"))
+
+
+if __name__ == "__main__":
+    main()
