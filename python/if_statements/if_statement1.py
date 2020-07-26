100052b5de860e9d38e322409ab6e274cd4c4883
Matthew Ambrose <mattbobambrose@gmail.com>
2020-07-26 13:16:47 -0700

    This should work

---
+++ python/if_statements/if_statement1.py
@@ -0,0 +1,17 @@
+def if_statements1(x):
+    if x > 10:
+        return x
+    return 0
+
+def main():
+    print(if_statements1(5))
+    print(if_statements1(15))
+    print(if_statements1(0))
+    print(if_statements1(10))
+    print(if_statements1(13))
+    print(if_statements1(20))
+    print(if_statements1(3))
+
+
+if __name__ == '__main__':
+    main()
